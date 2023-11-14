69ef221df63733a999a42084d44166b630b05aef
Finkch <skyler@thealdersons.org>
2023-11-14 15:48:58 -0800

    Integer addition now supports different byte counts

--- ints.lua
+++ ints.lua
@@ -1,6 +1,8 @@
 -- ints
 function int(vals)
-    local int = {frame = frame(vals)}
+    local int = {
+        frame = frame(vals)
+    }
 
     setmetatable(int, int_mt)
     return int
@@ -8,10 +10,48 @@ end
 
 function init_ints()
     int_mt = {
+
+        -- addition
         __add = function(a, b)
-            c = int()
+            local c = int()
 
             local carry = 0
+
+            -- 16-bit + n-bit case
+            if type(a) == "number" or type(b) == "number" then
+
+                -- enforces b to be the 16-bit number
+                if (type(a) == "number") a, b = b, a
+
+                -- iterates from LSB to MSB
+                for i = word_size - 1, 0, -1 do
+
+                    -- adds the bytes together
+                    local c_i = a.frame:get(i) + (b & 0xff) + carry
+
+                    carry = (c_i & ~0xff) >> 8 -- the overflow/carry
+                    local add = c_i & 0xff -- the value
+
+                    -- sets the byte in c
+                    c.frame:set(i, add)
+
+                    -- decrements b
+                    b = b >> 8
+
+                    -- hanldes the carry
+                    if i != 0 then
+                        c.frame:set(i - 1, carry)
+                    else
+                        assert(carry == 0, "overflow during addition: "..tostr(carry)..", "..(a.frame:get(i) + (b & 0xff) + carry))
+                    end
+                end
+
+
+                return c
+            end
+
+            -- n-bit + n-bit case
+
             -- iterates from LSB to MSB
             for i = word_size - 1, 0, -1 do
 
